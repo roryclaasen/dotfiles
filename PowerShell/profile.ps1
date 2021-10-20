d8ef9197ecee4b158310629aa722b169ce35fa0c
Rory Claasen <rory@xbox.com>
2021-10-20 09:48:35 +0100

    Set env flag

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -1,4 +1,7 @@
 Import-Module posh-git
+# Import-Module oh-my-posh
+
+$env:POSH_GIT_ENABLED = $true
 
 If ($host.Name -eq 'Visual Studio Code Host') {
     Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process
