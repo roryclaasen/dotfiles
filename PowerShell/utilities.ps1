a53ab231cbb41d95a3484c4057d49beb02d01413
Rory Claasen <rory@xbox.com>
2023-03-10 17:01:05 +0000

    Updates function

--- PowerShell/utilities.ps1
+++ PowerShell/utilities.ps1
@@ -14,12 +14,42 @@ function Check-LineEndings {
         [string]$Directory = $PWD
     )
 
-    Write-Host "Checking line endings in $Directory"
+    function Get-LineTypeForFile($file) {
+        try {
 
-    $files = Get-ChildItem -Path $Directory -Recurse -File
+            $content = Get-Content -Raw $file
+            $lineEndings = [regex]::Matches($content, "\r?\n") | Group-Object -Property Length
+            if ($lineEndings.Length -eq 2) {
+                return "Mixed CRLF"
+            }
+            else {
+                if ($lineEndings[0].Group[0].Value.Length -eq 2) {
+                    return "CRLF"
+                }
+                else {
+                    return "LF"
+                }
+            }
+        }
+        catch {
+            Write-Warning "Unable to check line endings for $($file.FullName)"
+            return "Unknown"
+        }
+    }
+
+    Write-Host "Checking line endings for files in $Directory"
+
+    $exclude = '\.git|\.vs|node_modules|packages|bin|obj|png|jpg|jpeg|gif|avif|webp|ico|ttf|woff|woff2|eot|svg|zip|rar|7z|gz|tar|bz2|exe|dll|pdb|bak|tmp|cache'
+    $output = @()
+
+    $files = Get-ChildItem -Path $Directory -Recurse -File | Where-Object { $_.FullName -notMatch $exclude }
     $files | ForEach-Object {
         $file = $_
-        $type = ('LF', 'CRLF')[([regex]::Matches($(Get-Content -Ra $file), "\r?\n") | Group-Object -P Length).Group[0].Value.Length - 1]
-        Write-Host "$file - $type" -ForegroundColor ([ConsoleColor]::Green)
+        $output += [PSCustomObject]@{
+            File = $file
+            Type = Get-LineTypeForFile($file)
+        }
     }
+
+    Write-Output $output
 }
