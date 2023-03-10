2c3f61ab14fc26c8c6ee3bd4ec6e6364ebe37d10
Rory Claasen <rory@xbox.com>
2023-03-10 16:27:12 +0000

    Adds Check-LineEndings function

--- PowerShell/utilities.ps1
+++ PowerShell/utilities.ps1
@@ -7,3 +7,19 @@ function colors() {
         Write-Host " on $bgcolor"
     }
 }
+
+function Check-LineEndings {
+    param(
+        [Parameter(Mandatory = $false)]
+        [string]$Directory = $PWD
+    )
+
+    Write-Host "Checking line endings in $Directory"
+
+    $files = Get-ChildItem -Path $Directory -Recurse -File
+    $files | ForEach-Object {
+        $file = $_
+        $type = ('LF', 'CRLF')[([regex]::Matches($(Get-Content -Ra $file), "\r?\n") | Group-Object -P Length).Group[0].Value.Length - 1]
+        Write-Host "$file - $type" -ForegroundColor ([ConsoleColor]::Green)
+    }
+}
