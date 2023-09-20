cc1599a941c7eb525b5d56a5f22114ef0372a562
Rory Claasen <rory@xbox.com>
2023-09-20 11:37:48 +0100

    Batch install should use powershell core where possible

--- install.bat
+++ install.bat
@@ -1,2 +1,9 @@
 @ECHO OFF
-powershell -ExecutionPolicy ByPass -NoProfile "%~dp0install.ps1" %*
+
+WHERE pwsh >nul 2>nul
+if (%ERRORLEVEL%) == (0) (
+    pwsh -ExecutionPolicy ByPass -NoLogo -NoProfile "%~dp0install.ps1" %*
+) else (
+    powershell -ExecutionPolicy ByPass -NoLogo -NoProfile "%~dp0install.ps1" %*
+)
+
