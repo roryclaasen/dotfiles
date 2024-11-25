3c0736fffa81d490523e9efea4927bc0c3d9110c
Rory Claasen <rory@xbox.com>
2024-11-25 16:59:32 +0000

    Remove posh-git customizations

--- PowerShell/terminal.ps1
+++ PowerShell/terminal.ps1
@@ -1,33 +1,14 @@
-$HasPoshGit = $false
 if (Get-Module -ListAvailable -Name posh-git) {
     Import-Module posh-git
-    $HasPoshGit = $true
 }
 
 if ($env:PG_ENVIRONMENT -eq 1) {
     # No theme
-} else {
-    $GitPromptSettings.DelimStatus.ForegroundColor = [ConsoleColor]::DarkGray
-    $GitPromptSettings.BeforeStatus.Text = [string]::Empty;
-    $GitPromptSettings.AfterStatus.Text = [string]::Empty;
-
+}
+else {
     $PoshTheme = Join-Path -Path $PSScriptRoot -ChildPath "roryclaasen.omp.json"
     if (Test-Path $PoshTheme) {
         oh-my-posh init pwsh --config $PoshTheme | Invoke-Expression
-    }
-
-    if ($HasPoshGit) {
-        function Set-PoshGitStatus {
-            $global:GitStatus = Get-GitStatus
-            if ($global:GitStatus) {
-                $env:POSH_GIT_STRING = (Write-GitStatus -Status $global:GitStatus).trim()
-            }
-            else {
-                $env:POSH_GIT_STRING = $null
-            }
-        }
-
-        New-Alias -Name 'Set-PoshContext' -Value 'Set-PoshGitStatus' -Scope Global -Force
     }
 }
 
