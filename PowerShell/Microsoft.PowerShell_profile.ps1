a364436ea3a62308938554b0f8ae16618b349f5e
Rory Claasen <rory@claasen.biz>
2025-10-31 18:28:56 +0000

    Add argument completer for 'nuke' command to enhance command-line usability

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -103,6 +103,17 @@ function prompt {
             # gh completion
             Invoke-Expression -Command $(gh completion -s powershell | Out-String)
         }
+    },
+    {
+        # Nuke
+        if (Get-Command nuke -ErrorAction SilentlyContinue) {
+            Register-ArgumentCompleter -Native -CommandName nuke -ScriptBlock {
+                param($commandName, $wordToComplete, $cursorPosition)
+                nuke :complete "$wordToComplete" | ForEach-Object {
+                    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+                }
+            }
+        }
     }
 )
 
