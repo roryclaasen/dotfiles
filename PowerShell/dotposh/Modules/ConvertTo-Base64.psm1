b942cd2cd8efe5dae250338843cd173b5e807b35
Rory Claasen <rory@claasen.biz>
2026-03-06 19:40:11 +0000

    Add ConvertFrom-Base64 function and export it for module usage

--- PowerShell/dotposh/Modules/ConvertTo-Base64.psm1
+++ PowerShell/dotposh/Modules/ConvertTo-Base64.psm1
@@ -8,7 +8,18 @@ function ConvertTo-Base64 {
     return [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($InputObject))
 }
 
+function ConvertFrom-Base64 {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline, Mandatory = $true)]
+        $InputObject
+    )
+
+    return [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($InputObject))
+}
+
 Set-Alias 'base64' 'ConvertTo-Base64'
 
 Export-ModuleMember -Function ConvertTo-Base64
+Export-ModuleMember -Function ConvertFrom-Base64
 Export-ModuleMember -Alias 'base64'
