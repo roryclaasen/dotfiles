4791c2d059ed7189a0543c5d0272922f79377d9d
Rory Claasen <rory@claasen.biz>
2026-03-08 21:47:10 +0000

    Refactor ConvertTo-Base64 and ConvertFrom-Base64 functions for improved handling of input objects

--- PowerShell/dotposh/Modules/ConvertTo-Base64.psm1
+++ PowerShell/dotposh/Modules/ConvertTo-Base64.psm1
@@ -5,7 +5,30 @@ function ConvertTo-Base64 {
         $InputObject
     )
 
-    return [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($InputObject))
+    begin {
+        $lines = [System.Collections.Generic.List[object]]::new()
+    }
+
+    process {
+        $lines.Add($InputObject)
+    }
+
+    end {
+        if ($lines.Count -eq 1 -and $lines[0] -is [byte[]]) {
+            return [Convert]::ToBase64String($lines[0])
+        }
+        else {
+            $isFormatData = $lines[0].GetType().FullName -like 'Microsoft.PowerShell.Commands.Internal.Format.*'
+            if ($isFormatData) {
+                $inputContents = ($lines | Out-String).TrimEnd()
+            }
+            else {
+                $inputContents = [string]::Join([Environment]::NewLine, $lines)
+            }
+
+            return [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($inputContents))
+        }
+    }
 }
 
 function ConvertFrom-Base64 {
@@ -15,7 +38,18 @@ function ConvertFrom-Base64 {
         $InputObject
     )
 
-    return [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($InputObject))
+    begin {
+        $lines = [System.Collections.Generic.List[string]]::new()
+    }
+
+    process {
+        $lines.Add("$InputObject")
+    }
+
+    end {
+        $inputContents = [string]::Concat($lines).Trim()
+        return [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($inputContents))
+    }
 }
 
 Set-Alias 'base64' 'ConvertTo-Base64'
